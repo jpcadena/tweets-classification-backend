757c21b09192ce183d3108e52017c0be3854da9a
Juan Pablo Cadena Aguilar <jpcadena@espol.edu.ec>
2023-03-05 20:54:02 -0500

    Added Alembic migration

---
+++ scripts/versions/344df8e66d27_create_model_table.py
@@ -0,0 +1,21 @@
+"""create model table
+
+Revision ID: 344df8e66d27
+Revises: 43da411d6c57
+Create Date: 2023-03-05 20:50:17.442668
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '344df8e66d27'
+down_revision = '43da411d6c57'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ scripts/versions/384055f89fc2_create_users_table.py
@@ -0,0 +1,21 @@
+"""create users table
+
+Revision ID: 384055f89fc2
+Revises: 
+Create Date: 2023-03-05 20:49:15.798005
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '384055f89fc2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
